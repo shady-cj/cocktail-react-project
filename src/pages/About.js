64ec04df599a1deea0a3bde6d84b247cec6b9c50
shady-cj <petersp2000@gmail.com>
2021-12-11 15:53:06 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <section className="setion about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Distinctio dolore dolores id fuga tenetur laborum, beatae iure
+                dignissimos mollitia autem eaque obcaecati! Explicabo tempora
+                laudantium quibusdam placeat tenetur reprehenderit dolorem modi
+                odio quis doloribus quod asperiores consequuntur accusamus,
+                excepturi molestias.
+            </p>
+        </section>
+    );
+};
+
+export default About;
